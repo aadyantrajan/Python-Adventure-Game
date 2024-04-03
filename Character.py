f846ddbd6f1f205cabcfae48c1a7583fa83eb6fb
Aadyant Rajan <aady.rajan@gmail.com>
2024-04-02 18:38:20 -0700

    Added variables to Class: Character

--- Character.py
+++ Character.py
@@ -1,3 +1,8 @@
 class Character(object):
-    def __init__(self):
-        level = 1
+    def __init__(self,name='',max_health=200,inventory=30,level=1,damage_modifier=1.0):
+        self.name = name
+        self.max_health = max_health
+        self.inventory = inventory
+        self.damage_modifier = damage_modifier
+        self.level = level
+        self.health = max_health
