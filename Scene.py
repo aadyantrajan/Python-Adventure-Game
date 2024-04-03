cdd4fafcbcdac8c0ea1a5bdfc7f94b225dabbbb3
Aadyant Rajan <aady.rajan@gmail.com>
2024-04-02 18:47:00 -0700

    Fixed errors in Scene.py

--- Scene.py
+++ Scene.py
@@ -5,12 +5,12 @@ class Scene(object):
         self.height = height
         self.npcs = {}
         for npc in range(npc_count):
-            npcs[npc] = (random(0, self.width), random(0, self.height))
+            self.npcs[npc] = (random.randint(0, self.width), random.randint(0, self.height))
         self.players = {}
         for player in range(player_count):
-            players[player] = (random(0, self.width), random(0, self.height))
+            self.players[player] = (random.randint(0, self.width), random.randint(0, self.height))
 
-    def update_player_loc(player, direction):
-        x, y = players[player]
+    def update_player_loc(self, player, direction):
+        x, y = self.players[player]
         newx, newy = direction
-        players[player] = (x + newx, y + newy)
+        self.players[player] = (x + newx, y + newy)
