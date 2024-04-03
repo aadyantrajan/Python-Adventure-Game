ebd359c28341da826908a0ef5249390449929551
Liam Petrie <liamdpetrie@vivaldi.net>
2024-04-02 18:52:36 -0700

    Add Tavern location

--- Scene.py
+++ Scene.py
@@ -14,3 +14,16 @@ class Scene(object):
         x, y = self.players[player]
         newx, newy = direction
         self.players[player] = (x + newx, y + newy)
+
+class Tavern(Scene):
+    def __init__(self, width, height, npc_count, player_count):
+        self.width = width
+        self.height = height
+        self.npcs = {}
+        for npc in range(npc_count):
+            self.npcs[npc] = (random.randint(0, self.width), random.randint(0, self.height))
+        self.npcs["bartender"] = (int(self.width / 2), int(self.height / 2))
+        self.players = {}
+        for player in range(player_count):
+            self.players[player] = (random.randint(0, self.width), random.randint(0, self.height))
+
